c724bba2ace04c986b40e204775dd0b7c73749f9
kaitobq <kaitobq520@gmail.com>
2024-10-09 19:40:18 +0900

    toastを実装

--- components/auth/SigninForm.tsx
+++ components/auth/SigninForm.tsx
@@ -3,16 +3,17 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button, Input, Label } from "components/ui"
 import { useAuth } from "hooks/useAuth"
+import { useToast } from "hooks/useToast"
 import React, { useState } from "react"
 import { Controller, useForm } from "react-hook-form"
 import { z } from "zod"
 
 const schema = z.object({
   email: z
-    .string()
-    .email({ message: "正しいmメールアドレスを入力してください。" }),
+    .string({ required_error: "メールアドレスを入力してください。", invalid_type_error: "入力値に誤りがあります。" })
+    .email({ message: "正しいメールアドレスを入力してください。" }),
   password: z
-    .string()
+    .string({ required_error: "パスワードを入力してください。", invalid_type_error: "入力値に誤りがあります。" })
     .min(8, { message: "パスワードは8文字以上で入力してください。" }),
 })
 
@@ -25,7 +26,15 @@ export const SigninForm = () => {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<schemaType>({ resolver: zodResolver(schema) })
+  } = useForm<schemaType>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+        email: "",
+        password: "",
+    }
+  })
+  const showErrorToast = useToast("error")
+  const showSuccessToast = useToast("success")
 
   const onSubmit = async (data: schemaType) => {
     setLoading(true)
@@ -33,8 +42,10 @@ export const SigninForm = () => {
     try {
       const res = await auth.signin(data)
       console.log(res)
+      showSuccessToast("success")
     } catch (error) {
       console.error(error)
+      showErrorToast("error")
     } finally {
       setLoading(false)
     }
@@ -43,39 +54,44 @@ export const SigninForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 m-3">
       <div className="space-y-2">
-        <Label htmlFor="email">Email</Label>
+        <Label htmlFor="email">メールアドレス</Label>
         <Controller
           control={control}
           render={({ field: { onChange, value } }) => (
             <Input
               id="email"
-              type="email"
-              placeholder="Enter your email"
+              placeholder="someone@example.com"
               onChange={onChange}
               value={value}
             />
           )}
           name="email"
         />
+        {errors.email && (
+            <p className="text-red-500 text-xs">{errors.email.message}</p>
+        )}
       </div>
       <div className="space-y-2">
-        <Label htmlFor="password">Password</Label>
+        <Label htmlFor="password">パスワード</Label>
         <Controller
           control={control}
           render={({ field: { onChange, value } }) => (
             <Input
               id="password"
               type="password"
-              placeholder="Enter your password"
+              placeholder="password"
               onChange={onChange}
               value={value}
             />
           )}
           name="password"
         />
+        {errors.password && (
+            <p className="text-red-500 text-xs">{errors.password.message}</p>
+        )}
       </div>
       <Button type="submit" className="w-full" disabled={loading}>
-        {loading ? "Creating Account..." : "Sign Up"}
+        {loading ? "ロード中..." : "サインイン"}
       </Button>
     </form>
   )
