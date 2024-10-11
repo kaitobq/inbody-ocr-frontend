1ca4a34a94a1d2aad3b6ea96ba7155a3235dc53c
kaitobq <kaitobq520@gmail.com>
2024-10-11 15:48:08 +0900

    fix lint

--- mods/repositories/auth.ts
+++ mods/repositories/auth.ts
@@ -1,47 +1,54 @@
-import type { CreateOrganizationRequest, CreateOrganizationResponse, SigninRequest, SigninResponse, SignupRequest, SignupResponse } from "types/auth"
+import type {
+  CreateOrganizationRequest,
+  CreateOrganizationResponse,
+  SigninRequest,
+  SigninResponse,
+  SignupRequest,
+  SignupResponse,
+} from "types/auth"
 import { fetcher } from "./fetcher"
 
 type Response = {
-    createOrgRes: CreateOrganizationResponse
-    signupRes: SignupResponse
-    signinRes: SigninResponse
+  createOrgRes: CreateOrganizationResponse
+  signupRes: SignupResponse
+  signinRes: SigninResponse
 }
 
 export const createOrganization = async (req: CreateOrganizationRequest) => {
-    const apiFetcher = fetcher()
-    return await apiFetcher.fetchJSON<Response["createOrgRes"]>({
-        path: "/v1/organization",
-        method: "POST",
-        body: {
-            "user_name": req.userName,
-            "email": req.email,
-            "password": req.password,
-            "organization_name": req.organizationName
-        }
-    })
+  const apiFetcher = fetcher()
+  return await apiFetcher.fetchJSON<Response["createOrgRes"]>({
+    path: "/v1/organization",
+    method: "POST",
+    body: {
+      user_name: req.userName,
+      email: req.email,
+      password: req.password,
+      organization_name: req.organizationName,
+    },
+  })
 }
 
 export const signup = async (orgId: string, req: SignupRequest) => {
-    const apiFetcher = fetcher()
-    return await apiFetcher.fetchJSON<Response["signupRes"]>({
-        path: `/v1/organization/${orgId}/signup`,
-        method: "POST",
-        body: {
-            "name": req.name,
-            "email": req.email,
-            "password": req.password,
-        }
-    })
+  const apiFetcher = fetcher()
+  return await apiFetcher.fetchJSON<Response["signupRes"]>({
+    path: `/v1/organization/${orgId}/signup`,
+    method: "POST",
+    body: {
+      name: req.name,
+      email: req.email,
+      password: req.password,
+    },
+  })
 }
 
 export const signin = async (req: SigninRequest) => {
-    const apiFetcher = fetcher()
-    return await apiFetcher.fetchJSON<Response["signinRes"]>({
-        path: "/v1/organization/signin",
-        method: "POST",
-        body: {
-            "email": req.email,
-            "password": req.password
-        }
-    })
+  const apiFetcher = fetcher()
+  return await apiFetcher.fetchJSON<Response["signinRes"]>({
+    path: "/v1/organization/signin",
+    method: "POST",
+    body: {
+      email: req.email,
+      password: req.password,
+    },
+  })
 }
--- mods/repositories/fetcher.ts
+++ mods/repositories/fetcher.ts
@@ -1,50 +1,50 @@
-import type { Body, Req, RespConvert } from 'types/fetcher'
+import type { Body, Req, RespConvert } from "types/fetcher"
 
 const API_BASE_URL = "http://localhost:8080/api"
 
 export const fetcher = () => {
-    const fetchJSON = async<T, U = T>(req: Req, convert?: RespConvert<T, U>) => {
-        const method = req.method
-        const headers = req.header || {}
-        headers["Content-Type"] = "application/json"
-        const body = (() => {
-            if (req.method === "POST" || req.method === "PUT") {
-                return makeBody(req.body)
-            }
-            return undefined
-        })()
+  const fetchJSON = async <T, U = T>(req: Req, convert?: RespConvert<T, U>) => {
+    const method = req.method
+    const headers = req.header || {}
+    headers["Content-Type"] = "application/json"
+    const body = (() => {
+      if (req.method === "POST" || req.method === "PUT") {
+        return makeBody(req.body)
+      }
+      return undefined
+    })()
 
-        const res = await fetch(`${API_BASE_URL + req.path}`, {
-            method: method,
-            headers: headers,
-            body: body,
-        })
-        .then(res => res.text())
-        .then(text => {
-            const data = JSON.parse(text)
-            const converted = convert ? convert(data) : (data as U)
-            return converted
-        })
+    const res = await fetch(`${API_BASE_URL + req.path}`, {
+      method: method,
+      headers: headers,
+      body: body,
+    })
+      .then((res) => res.text())
+      .then((text) => {
+        const data = JSON.parse(text)
+        const converted = convert ? convert(data) : (data as U)
+        return converted
+      })
 
-        return res
-    }
+    return res
+  }
 
-    // const fetchFormData
+  // const fetchFormData
 
-    return {
-        fetchJSON
-    }
+  return {
+    fetchJSON,
+  }
 }
 
 const makeBody = (param: Body): string => {
-    if(param === undefined) {
-        return JSON.stringify({})
-    }
+  if (param === undefined) {
+    return JSON.stringify({})
+  }
 
-    const key = Object.keys(param)
-    if(key.length === 0) {
-        return JSON.stringify({})
-    }
+  const key = Object.keys(param)
+  if (key.length === 0) {
+    return JSON.stringify({})
+  }
 
-    return JSON.stringify(param)
+  return JSON.stringify(param)
 }
