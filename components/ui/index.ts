0865dab395e81f594713ab777d73740e70679656
kaitobq <kaitobq520@gmail.com>
2024-10-12 18:06:37 +0900

    add ui component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,10 @@
 export * from "./button"
 export * from "./input"
 export * from "./label"
+export * from "./card"
+export * from "./table"
+export * from "./checkbox"
+export * from "./select"
+export * from "./radio-group"
+export * from "./tabs"
+
