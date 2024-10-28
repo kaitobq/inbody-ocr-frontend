a2bd1c93028a22598227c7f83f4825b5ec86ed4a
kaitobq <kaitobq520@gmail.com>
2024-10-28 11:41:10 +0900

    statsCardの共通化

--- components/dashboard/admin/AdminDashboard.tsx
+++ components/dashboard/admin/AdminDashboard.tsx
@@ -5,7 +5,7 @@ import type React from "react"
 import type { GetScreenForAdminResponse } from "types/dashboard"
 import { OrgDataChart } from "./OrgDataChart"
 import { ScoreRanking } from "./ScoreRanking"
-import { StatCard } from "./StatCard"
+import { type StatCardProps, Stats } from "../StatCard"
 import { UserDataChart } from "./UserDataChart"
 import { UserDistributionChart } from "./UserDistributionChart"
 import { UserTable } from "./UserTable"
@@ -17,31 +17,36 @@ interface Props {
 export function AdminDashboard(props: Props) {
   const { data } = props
 
+  const userIcon = <Users className="h-4 w-4 text-muted-foreground" />
+  const stats: StatCardProps[] = [
+    {
+      title: "平均体重",
+      value: `${data.avg.weight.toFixed(1)} kg`,
+      icon: userIcon
+    },
+    {
+      title: "平均筋肉量",
+      value: `${data.avg.muscle_weight.toFixed(1)} kg`,
+      icon: userIcon
+    },
+    {
+      title: "平均体脂肪率",
+      value: `${data.avg.fat_percent.toFixed(1)}%`,
+      icon: userIcon
+    },
+    {
+      title: "平均得点",
+      value: data.avg.point,
+      icon: userIcon
+    }
+  ]
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">管理者ダッシュボード</h1>
 
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <StatCard
-          title="平均体重"
-          value={`${data.avg?.weight.toFixed(1)} kg`}
-          icon={<User className="h-4 w-4 text-muted-foreground" />}
-        />
-        <StatCard
-          title="平均筋肉量"
-          value={`${data.avg?.muscle_weight.toFixed(1)} kg`}
-          icon={<Users className="h-4 w-4 text-muted-foreground" />}
-        />
-        <StatCard
-          title="平均体脂肪率"
-          value={`${data.avg?.fat_percent.toFixed(1)}%`}
-          icon={<User className="h-4 w-4 text-muted-foreground" />}
-        />
-        <StatCard
-          title="平均得点"
-          value={data.avg?.point}
-          icon={<User className="h-4 w-4 text-muted-foreground" />}
-        />
+        <Stats stats={stats} />
       </div>
 
       {/* <OrgDataChart data={data} /> */}
--- components/dashboard/member/Dashboard.tsx
+++ components/dashboard/member/Dashboard.tsx
@@ -7,6 +7,7 @@ import type { GetScreenForMemberResponse } from "types/dashboard"
 import { CustomLineChart } from "./CustomLineChart"
 import { DataHistoryTable } from "./DataHistoryTable"
 import { ImageUploader } from "./ImageUploader"
+import { type StatCardProps, Stats } from "../StatCard"
 
 interface Props {
   data: GetScreenForMemberResponse
@@ -15,65 +16,34 @@ interface Props {
 export function Dashboard(props: Props) {
   const { data } = props
 
+  const userIcon = <User className="h-4 w-4 text-muted-foreground" />
+  const stats: StatCardProps[] = [
+    {
+      title: "現在の体重",
+      value: `${data.current.weight.toFixed(1)} kg`,
+      change: data.current.weight - data.previous.weight,
+      icon: userIcon
+    },
+    {
+      title: "現在の筋肉量",
+      value: `${data.current.muscle_weight.toFixed(1)} kg`,
+      change: data.current.muscle_weight - data.previous.muscle_weight,
+      icon: userIcon
+    },
+    {
+      title: "現在の体脂肪量",
+      value: `${data.current.fat_weight.toFixed(1)}%`,
+      change: data.current.fat_weight - data.previous.fat_weight,
+      icon: userIcon
+    }
+  ]
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">InBody データダッシュボード</h1>
 
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">現在の体重</CardTitle>
-            <User className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {data?.current?.weight.toFixed(1)} kg
-            </div>
-            <p className="text-xs text-muted-foreground">
-              前回より
-              {(data?.current?.weight - data?.previous?.weight).toFixed(1)}
-              kg 変化
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">現在の筋肉量</CardTitle>
-            <User className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {data?.current?.muscle_weight.toFixed(1)} kg
-            </div>
-            <p className="text-xs text-muted-foreground">
-              前回より
-              {(
-                data?.current?.muscle_weight - data?.previous?.muscle_weight
-              ).toFixed(1)}
-              kg 変化
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              現在の体脂肪量
-            </CardTitle>
-            <User className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {data?.current?.fat_weight.toFixed(1)} kg
-            </div>
-            <p className="text-xs text-muted-foreground">
-              前回より
-              {(data?.current?.fat_weight - data?.previous?.fat_weight).toFixed(
-                1,
-              )}
-              kg 変化
-            </p>
-          </CardContent>
-        </Card>
+        <Stats stats={stats} />
       </div>
 
       <CustomLineChart data={data} />
